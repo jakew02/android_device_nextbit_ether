0cf2a38df3508b8ecc66a154e2a21ee51876376a
Jacob Whatley <jake@whatley.us>
2017-11-29 16:57:00 +0100

    display: adapt for AOSP display HAL
    Change-Id: I04351c715d58eb044cd29ede00f44e1551fbf059

--- device.mk
+++ device.mk
@@ -82,6 +82,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.xml:system/etc/permissions/android.software.sip.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
 
+# Vendor Interface Manifest
+PRODUCT_COPY_FILES += \
+    device/nextbit/ether/manifest.xml:system/vendor/manifest.xml
+
 # ANT+
 PRODUCT_PACKAGES += \
     AntHalService \
@@ -145,13 +149,17 @@ PRODUCT_PACKAGES += \
 
 # Display
 PRODUCT_PACKAGES += \
-    copybit.msm8992 \
     gralloc.msm8992 \
     hwcomposer.msm8992 \
-    liboverlay \
-    libqdutils \
-    libqdMetaData \
     memtrack.msm8992 \
+    libgenlock
+
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.memtrack@1.0-impl
 
 # Extra tools
 PRODUCT_PACKAGES += \
