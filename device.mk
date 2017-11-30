240c622bd8d474daea9972a7253e3cbd6f487065
Jacob Whatley <jake@whatley.us>
2017-11-30 04:39:39 +0100

    audio: adapt for AOSP audio HAL
    Change-Id: Ia94bfc6ed0b91ffe404f3f0229819054fdfd5657

--- device.mk
+++ device.mk
@@ -99,17 +99,22 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
     audio.primary.msm8992 \
-    audio_policy.msm8992 \
     audio.r_submix.default \
     audio.usb.default \
     audio_amplifier.msm8992
 
 PRODUCT_PACKAGES += \
-    audiod \
     libaudio-resampler \
     libqcompostprocbundle \
     libqcomvisualizer \
-    libqcomvoiceprocessing
+    libqcomvoiceprocessing \
+    libvolumelistener
+
+PRODUCT_PACKAGES += \
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio.effect@2.0-impl \
+    android.hardware.soundtrigger@2.0-impl \
+    android.hardware.broadcastradio@1.0-impl
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/aanc_tuning_mixer.txt:system/etc/aanc_tuning_mixer.txt \
@@ -163,10 +168,6 @@ PRODUCT_PACKAGES += \
 
 # Extra tools
 PRODUCT_PACKAGES += \
-    tinycap \
-    tinymix \
-    tinypcminfo \
-    tinyplay \
     libtinyxml
 
 #for android_filesystem_config.h
